c7ef3faccb588bbc7d822eb077f5ff5e2077ed61
Supriya06-code <supriyanjadhav2000@gmail.com>
2024-09-25 20:09:01 +0530

    server added

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,38 @@
 import express from "express";
 import dotenv from 'dotenv'
 import dbConnect from "./DB/dbConnect.js";
-const app = express();
+import authRouter from  './rout/authUser.js'
+// import messageRouter from './rout/messageRout.js'
+// import userRouter from './rout/userRout.js'
+// import cookieParser from "cookie-parser";
+import path from "path";
+// import {app , server} from './Socket/socket.js'
+import { fileURLToPath } from 'url'; // Import for converting URL to path
+import { dirname } from 'path'; // Import to get the directory name
+import { createServer } from "http";
 
 dotenv.config();
+// Define __dirname in ES modules
+const __filename = fileURLToPath(import.meta.url); // Get the file name
+const __dirname = dirname(__filename); // Get the directory name
+const app = express();
 
-app.get('/',(req,res)=>{
-    res.send("Server is working")
+app.use(express.json());
+
+
+app.use('/api/auth',authRouter)
+// app.use('/api/message',messageRouter)
+// app.use('/api/user',userRouter)
+
+app.use(express.static(path.join(__dirname,"frontend","dist")))
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
 })
-
+const server = createServer(app);
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT,()=>{
+server.listen(PORT,()=>{
     dbConnect();
-    console.log(`working at ${PORT}`);
+    console.log(`Working at ${PORT}`);
 })
