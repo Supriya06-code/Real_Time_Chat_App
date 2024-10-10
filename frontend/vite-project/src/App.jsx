4764699bde3fdcbca887e3c24cefc5bf794cdaa2
Supriya06-code <supriyanjadhav2000@gmail.com>
2024-10-10 12:31:13 +0530

    frontend libraries install

---
+++ frontend/vite-project/src/App.jsx
@@ -0,0 +1,18 @@
+
+
+function App() {
+  
+
+  return (
+    <>
+    <div className="p-2 w-screen h-screen flex items-center justify-center">
+
+    </div>
+    {/* <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1> */}
+      </>
+  )
+}
+
+export default App
