d0e40d6c859ea1e352e076a151905a7999c71d21
Supriya06-code <supriyanjadhav2000@gmail.com>
2024-10-13 21:32:31 +0530

    login structure removed

--- frontend/vite-project/src/login/Login.jsx
+++ frontend/vite-project/src/login/Login.jsx
@@ -1,33 +1,33 @@
-import React from 'react';
+// import React from 'react';
 
-const Login = () => {
-  return (
-    <div className='flex flex-col items-center justify-center max-w-full mx-auto'>
-      <div className='w-full p-6 rounded-lg shadow-lg bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-50'>
-        <h1 className='text-3xl font-bold text-center text-gray-300 mb-4'>
-          Login<span className='text-gray-950'>TalkCircle</span>
-        </h1>
-        <form className='flex flex-col space-y-4'>
-          <input
-            type='text'
-            placeholder='Username'
-            className='p-2 rounded border border-gray-300'
-          />
-          <input
-            type='password'
-            placeholder='Password'
-            className='p-2 rounded border border-gray-300'
-          />
-          <button
-            type='submit'
-            className='w-full p-2 rounded bg-purple-600 text-white hover:bg-blue-700 transition duration-300'
-          >
-            Login
-          </button>
-        </form>
-      </div>
-    </div>
-  );
-};
+// const Login = () => {
+//   return (
+//     <div className='flex flex-col items-center justify-center max-w-full mx-auto'>
+//       <div className='w-full p-6 rounded-lg shadow-lg bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-50'>
+//         <h1 className='text-3xl font-bold text-center text-gray-300 mb-4'>
+//           Login<span className='text-gray-950'>TalkCircle</span>
+//         </h1>
+//         <form className='flex flex-col space-y-4'>
+//           <input
+//             type='text'
+//             placeholder='Username'
+//             className='p-2 rounded border border-gray-300'
+//           />
+//           <input
+//             type='password'
+//             placeholder='Password'
+//             className='p-2 rounded border border-gray-300'
+//           />
+//           <button
+//             type='submit'
+//             className='w-full p-2 rounded bg-purple-600 text-white hover:bg-blue-700 transition duration-300'
+//           >
+//             Login
+//           </button>
+//         </form>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default Login;
+// export default Login;
