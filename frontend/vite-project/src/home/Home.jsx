197aef9c92159c8d36fbae45267e58b70cba7a66
Supriya06-code <supriyanjadhav2000@gmail.com>
2024-10-31 22:18:21 +0530

    new component added

--- frontend/vite-project/src/home/Home.jsx
+++ frontend/vite-project/src/home/Home.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import { useAuth } from '../context/AuthContext';
+import Sidebar from './components/Sidebar';
+import MessageContainer from './components/MessageContainer';
 const Home = () => {
     const {authUser} = useAuth();
     
   return (
-    <div className='text-3xl'>
-      hiii {authUser?.username}
+    <div className="flex flex-col items-center justify-center max-w-full mx-auto">
+      <div className="w-full p-6 rounded-lg shadow-lg bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-50">
+
+        <div>
+<Sidebar/>
+        </div>
+        <div>
+<MessageContainer/>
+        </div>
+      </div>
     </div>
   )
 }
---
+++ frontend/vite-project/src/home/components/Sidebar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div className='h-full w-auto px-1'>
+      <div className='flex justify-between gap-2'>
+        <form className='w-auto flex items-center justify-between bg-white rounded-full'>
+            <input/>
+    <button className='btn btn-circle bg-sky-700 hover:bg-gray-950'></button>
+        </form>
+
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
