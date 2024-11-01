25775670dac3c5d14cb5c5855d0dd240526e9396
Supriya06-code <supriyanjadhav2000@gmail.com>
2024-11-01 21:57:50 +0530

    sidebar commented

--- frontend/vite-project/src/home/components/Sidebar.jsx
+++ frontend/vite-project/src/home/components/Sidebar.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 
 const Sidebar = () => {
-  return (
-    <div className='h-full w-auto px-1'>
-      <div className='flex justify-between gap-2'>
-        <form className='w-auto flex items-center justify-between bg-white rounded-full'>
-            <input/>
-    <button className='btn btn-circle bg-sky-700 hover:bg-gray-950'></button>
-        </form>
+  // return (
+  //   <div className='h-full w-auto px-1'>
+  //     <div className='flex justify-between gap-2'>
+  //       <form className='w-auto flex items-center justify-between bg-white rounded-full'>
+  //           <input/>
+  //   <button className='btn btn-circle bg-sky-700 hover:bg-gray-950'></button>
+  //       </form>
 
-      </div>
-    </div>
-  )
+  //     </div>
+  //   </div>
+  // )
 }
 
 export default Sidebar
