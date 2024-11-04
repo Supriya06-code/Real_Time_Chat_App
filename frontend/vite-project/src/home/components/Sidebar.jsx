5e6083674900f83e9bb714f78706d7edea6c96ad
Supriya06-code <supriyanjadhav2000@gmail.com>
2024-11-04 20:23:00 +0530

    sidebar search function and profile image added

--- frontend/vite-project/src/home/components/Sidebar.jsx
+++ frontend/vite-project/src/home/components/Sidebar.jsx
@@ -1,17 +1,62 @@
 import React from 'react'
-
+import{FaSearch} from 'react-icons/fa'
+import axios from 'axios'
+import { useState } from 'react'
+import {toast} from 'react-toastify'
+import{useNavigate} from 'react-router-dom'
+import { useAuth } from '../../context/AuthContext';
 const Sidebar = () => {
-  // return (
-  //   <div className='h-full w-auto px-1'>
-  //     <div className='flex justify-between gap-2'>
-  //       <form className='w-auto flex items-center justify-between bg-white rounded-full'>
-  //           <input/>
-  //   <button className='btn btn-circle bg-sky-700 hover:bg-gray-950'></button>
-  //       </form>
+  const navigate = useNavigate();
+  const {authUser} = useAuth();
+  
+  const [searchInput, setSearchInput] = useState('')
+  const[searchUser, setSearchUser] =useState([])
+  const[loading, setLoading] =useState(false)
+  const handleSearchSubmit=async(e)=>{
+e.preventDefault();
+setLoading(true)
+try{
+const search = await axios.get(`/api/user/search?search=${searchInput}`);
+const data=search.data;
+if(data.success===false){
+  setLoading(false)
+  console.log(data.message)
+}
+setLoading(false)
+if(data.loading === 0){
+  toast.info("User Not Found")
+}else{
+  setSearchUser(data)
+}
+}catch(error){
+setLoading(false)
+console.log(error);
+}
+  }
+  console.log(searchUser);
+  console.log("authUser:", authUser);
+  console.log("Profile Picture URL:", authUser?.profilepic);
 
-  //     </div>
-  //   </div>
-  // )
+  return (
+    <div className='h-full w-auto px-1'>
+      <div className='flex justify-between gap-2'>
+        <form onSubmit={handleSearchSubmit}className='w-auto flex items-center justify-between bg-white rounded-full'>
+            <input value={searchInput}
+            onChange={(e)=>setSearchInput(e.target.value)} type="text" className='px-4 w-auto bg-transparent outline-none rouned-full' placeholder='search user'/>
+           
+    <button className='btn btn-circle bg-sky-700 hover:bg-gray-950'>
+    <FaSearch />
+    </button>
+    
+        </form>
+        <img onClick={()=>navigate(`/profile/${authUser._id}`)}src={authUser?.profilepic}
+    className='self-center h-12 w-12 hover:scale-110 cursor-pointer'/>
+    
+
+      </div>
+      <div className='divider px-3'></div>
+    </div>
+  )
 }
 
 export default Sidebar
