15854d9a30e000de5bf8b3b56d1490d24f78d488
Supriya06-code <supriyanjadhav2000@gmail.com>
2024-11-12 11:42:44 +0530

    **

--- frontend/vite-project/vite.config.js
+++ frontend/vite-project/vite.config.js
@@ -1,15 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server:{
+//     proxy:{
+//       '/api':{
+//         target:'http://localhost:3000',
+//         secure:false
+//       }
+//     }
+//   }
+// })
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    proxy:{
-      '/api':{
-        target:'http://localhost:3000',
-        secure:false
-      }
-    }
-  }
-})
+  build: {
+    outDir: 'frontend/dist',  // Specify the build output folder
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000', // Proxy for API requests to backend
+        secure: false,
+      },
+    },
+  },
+});
